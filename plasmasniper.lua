3eb3f4af6821047d1a92b7a8f42237572e5ea04d
OverZuka <hotspotgaming123@gmail.com>
2026-02-04 22:58:59 -0600

    Create plasmasniper.lua

---
+++ plasmasniper.lua
@@ -0,0 +1,54 @@
+local targetModule = require(game:GetService("Players").OverZuka.Backpack.PlasmaSniper.Setting["1"])
+if setreadonly then setreadonly(targetModule, false) end
+
+targetModule.AngleX_Min = 0 
+targetModule.Lifesteal = 99999 
+targetModule.MeleeCriticalDamageEnabled = 999999 
+targetModule.BaseDamage = 999999 
+targetModule.MeleeDamage = 999999 
+targetModule.ChargingTime = 0 
+targetModule.BulletSpeed = 90000 
+targetModule.HeadshotEnabled = 100 
+targetModule.AngleZ_Max = 0 
+targetModule.DelayBeforeFiring = 0 
+targetModule.EquipTime = 0 
+targetModule.BurstRate = 0 
+targetModule.Recoil = 0 
+targetModule.MeleeHeadshotEnabled = 100 
+targetModule.HoldAndReleaseEnabled = false 
+targetModule.AngleX_Max = 0 
+targetModule.CriticalDamageEnabled = 999999 
+targetModule.ShotgunEnabled = true 
+targetModule.Knockback = 9999 
+targetModule.SuperRicochet = false 
+targetModule.AmmoPerMag = 999999 
+targetModule.FireRate = 0 
+targetModule.MeleeCriticalDamageMultiplier = 999999 
+targetModule.RicochetAmount = 0 
+targetModule.ZeroDamageDistance = 999999 
+targetModule.AngleY_Max = 0 
+targetModule.DualFireEnabled = true 
+targetModule.MeleeHeadshotDamageMultiplier = 999999 
+targetModule.CriticalDamageMultiplier = 999999 
+targetModule.TacticalReloadTime = 0 
+targetModule.DelayAfterFiring = 0 
+targetModule.ReduceSelfDamageOnAirOnly = 999999 
+targetModule.SelfDamage = 999999 
+targetModule.DamageBasedOnDistance = 999999 
+targetModule.ExplosionRadius = 9999 
+targetModule.DamageDropOffEnabled = 999999 
+targetModule.SwitchTime = 0 
+targetModule.ReloadTime = 0 
+targetModule.BulletPerShot = 5 
+targetModule.FriendlyFire = true 
+targetModule.AngleY_Min = 0 
+targetModule.SilenceEffect = true 
+targetModule.Accuracy = 0 
+targetModule.LimitedAmmoEnabled = false 
+targetModule.FullDamageDistance = 999999 
+targetModule.HeadshotDamageMultiplier = 999999 
+targetModule.AngleZ_Min = 0 
+targetModule.SelfDamageRedution = 999999 
+
+if setreadonly then setreadonly(targetModule, true) end
+print('UPGRADED.')
